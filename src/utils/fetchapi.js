ad4c3cdb00ed767077f9f7ea99fb799eadbf3ba8
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-10-21 18:40:13 +0530

    fetchApi

--- src/utils/fetchapi.js
+++ src/utils/fetchapi.js
@@ -7,7 +7,7 @@ const options = {
     maxResults: "50",
   },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YT_API_KEY,
+    "X-RapidAPI-Key": "2feae43139msh91d8e953f8663a7p1aa1bcjsne33d5897f08b",
     "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
   },
 };
